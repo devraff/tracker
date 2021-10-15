200db5cb5dbe53bf881699b4143c166176c80c30
YatoDev <rafael-silva_f5@hotmail.com>
2021-10-15 03:10:53 -0300

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from '../styles/globalstyle';
+import { theme } from '../styles/theme';
+import '../styles/globals.css';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
