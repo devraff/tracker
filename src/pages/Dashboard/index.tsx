f7d339bc33dd2db5b3cdd0f519ebc52471d48879
devraff <rafael-silva_f5@hotmail.com>
2021-10-18 16:49:43 -0300

    add dynamic cards

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,17 +1,34 @@
 import { Button } from "../../components/atoms/Button"
 import { TemplateContainer } from "../../components/templates"
+import { Card } from "./components/Card"
 
 export const Dashboard = () => {
+  const array = [
+    { id: '0', title: 'Netflix', category: 'streamming', cost: 39.99 },
+    { id: '1', title: 'Amazon', category: 'streamming', cost: 9.99 },
+    { id: '2', title: 'Internet', category: 'web', cost: 29.99 },
+    { id: '3', title: 'Spotify', category: 'streaming', cost: 9.99 },
+  ];
+
   return (
     <TemplateContainer>
       <header className='w-full max-w-screen-lg flex justify-between items-center h-1/6'>
         <span className='font-bold text-2xl'>Expanse Tracker</span>
-
         <nav className='flex gap-4'>
         <Button>Adicionar Nova Conta</Button>
         <Button variant='ghost'>Sair</Button>
       </nav>
       </header>
+
+      <main className='w-full bg-purple-50 max-w-screen-lg h-5/6 grid overflow-y-scroll content-start grid-cols-3 gap-4 p-4 pb-8'>
+          {array.map((item) => (
+           <Card 
+           key={item.id} 
+           title={item.title} 
+           category={item.category}
+            cost={item.cost}/>
+          ))}
+      </main>
     </TemplateContainer>
   )
 }
